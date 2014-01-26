9b5b0966821c36f832311679d0b85fdd3f1d05cf
Francesco Canessa <makevoid@gmail.com>
2014-01-26 13:28:06 +0100

    added pages, initial texts and stubs

--- donacoin_web.rb
+++ donacoin_web.rb
@@ -5,32 +5,103 @@ require 'json'
 require 'bundler/setup'
 Bundler.require :default
 
-class DonacoinWeb < Sinatra::Application
+# todo: move in models / db
 
-  CAUSES = [
-    { name: :wikipedia, label: "Wikipedia", desc: "Wikipedia is the free encyclopedia" },
-    { name: :riotvan, label: "RiotVan", desc: "RiotVan is a free press magazine based in Firenze, Italy" },  
-  ]
-  
-  get "/" do
-    @causes = CAUSES
-    haml :index
-  end
+CAUSES = [
+  { name: :wikipedia, label: "Wikipedia", desc: "Wikipedia is the free encyclopedia" },
+  { name: :riotvan, label: "RiotVan", desc: "RiotVan is a free press magazine based in Firenze, Italy" },
+  { name: :fsf, label: "Free Software Foundation", desc: "RiotVan is a free press magazine based in Firenze, Italy" },
+]
 
-  get "/causes/:name" do |name|
-    @cause = CAUSES.find{ |c| c[:name].to_s == name  }
-    haml :cause
-  end
-  
-  # api
-  
-  get "/miner" do
-    content_type :json
+DONORS = [
+  { username: "makevoid" },
+  { username: "filipporetti" },
+  { username: "Virtuoid" },
+  { username: "wouldgo" },
+]
+
+class Pool
+  def self.current
     {
       pool: "stratum+tcp://dgc.hash.so:3341",
       worker_user: "donacoin.1",
       worker_pass: "1",
-    }.to_json
+    } # virtuo's account
+  end
+end
+
+class Cause
+  def self.all
+    CAUSES
+  end
+end
+
+class Donor
+  def self.all
+    DONORS
+  end
+
+  def self.top
+    DONORS
+  end
+end
+
+class DonacoinWeb < Sinatra::Application
+
+  get "/" do
+    @causes = Cause.all
+    haml :index
+  end
+
+  get "/download" do
+    haml :download
+  end
+
+  get "/causes" do
+    @causes = Cause.all
+    haml :causes
+  end
+
+  get "/causes/:name" do |name|
+    @cause = Cause.all.find{ |c| c[:name].to_s == name  }
+    haml :cause
+  end
+
+  get "/users" do
+    redirect "/donors"
+  end
+
+  get "/donors" do
+    @donors = Donor.all
+    haml :donors
+  end
+
+  get "/donors/:name" do |name|
+    @donor = Donor.all.find{ |c| c[:username].to_s == name  }
+    haml :donor
+  end
+
+  get "/guide" do
+    haml :guide
+  end
+
+  get "/terms" do
+    haml :terms
+  end
+
+  get "/privacy" do
+    haml :privacy
+  end
+
+  get "/contacts" do
+    haml :contacts
+  end
+
+  # api
+
+  get "/miner" do
+    content_type :json
+    Pool.current.to_json
   end
 
 end
